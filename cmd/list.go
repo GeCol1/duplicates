0ad757ac9aeb40a8908cefdc492399b977c44755
Gerald Colin <trash@goovy.com>
2022-05-04 18:12:24 +0200

    fix warnings

--- cmd/list.go
+++ cmd/list.go
@@ -16,7 +16,7 @@ func (*ListCmd) Name() string     { return "list" }
 func (*ListCmd) Synopsis() string { return "list the duplicated file from the second folder" }
 func (*ListCmd) Usage() string {
 	return `list -main <folder1> --source <folder2>
-	list the duplicates found from folder2 into folder1 and print in stdoud.`
+	list the duplicates found from folder2 into folder1 and print in stdout.`
 }
 
 func (l *ListCmd) SetFlags(f *flag.FlagSet) {
@@ -24,7 +24,7 @@ func (l *ListCmd) SetFlags(f *flag.FlagSet) {
 	f.StringVar(&l.source, "source", "", "add the source folder path")
 }
 
-func (l *ListCmd) Execute(ctx context.Context, f *flag.FlagSet, args ...interface{}) subcommands.ExitStatus {
+func (l *ListCmd) Execute(_ context.Context, _ *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
 	fmt.Printf("args : %s et %s", l.main, l.source)
 	return subcommands.ExitSuccess
 }
