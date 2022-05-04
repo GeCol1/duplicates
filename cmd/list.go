f944e5791b75cc3aa3f0f373e6551f0e4d865f5f
Gerald Colin <trash@goovy.com>
2022-05-04 18:42:00 +0200

    refactor with root command

--- cmd/list.go
+++ cmd/list.go
@@ -8,24 +8,24 @@ import (
 	"log"
 )
 
-type ListCmd struct {
+type listCmd struct {
 	main   string
 	source string
 }
 
-func (*ListCmd) Name() string     { return "list" }
-func (*ListCmd) Synopsis() string { return "list the duplicated file from the second folder" }
-func (*ListCmd) Usage() string {
+func (*listCmd) Name() string     { return "list" }
+func (*listCmd) Synopsis() string { return "list the duplicated file from the second folder" }
+func (*listCmd) Usage() string {
 	return `list -main <folder1> --source <folder2>
 	list the duplicates found from folder2 into folder1 and print in stdout.`
 }
 
-func (l *ListCmd) SetFlags(f *flag.FlagSet) {
+func (l *listCmd) SetFlags(f *flag.FlagSet) {
 	f.StringVar(&l.main, "main", "", "add the main folder path")
 	f.StringVar(&l.source, "source", "", "add the source folder path")
 }
 
-func (l *ListCmd) Execute(_ context.Context, _ *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
+func (l *listCmd) Execute(_ context.Context, _ *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
 	log.Printf("execute command with main : %s and source : %s", l.main, l.source)
 	err := core.FindDuplicates(l.main, l.source)
 	if err != nil {
